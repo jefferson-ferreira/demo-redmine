5dccaa05191ce1d6687ce2ce88dff1a5c5721de4
Jefferson Ferreira <jefferson_ferreira@outlook.com>
2018-08-02 09:58:57 -0300

    Inclusão dos arquivos de teste

---
+++ test/test.js
@@ -0,0 +1,96 @@
+var test = require('selenium-webdriver/testing');
+var driver;
+var homePage;
+var indexPage;
+
+var IndexPage = require('./page-objects/index-page');
+var HomePage = require('./page-objects/home-page');
+var RegisterPage = require('./page-objects/register-page');
+var LoginPage = require('./page-objects/login-page');
+var ProjectPage = require('./page-objects/project-page');
+var NewProjectPage = require('./page-objects/new-project-page');
+
+const timeOut = 30000;
+
+test.describe('Demo-Redmine', function () {
+
+    var usuario = 'jefferson.ferreira';
+	var senha = senha;
+    var projeto = 'Prova-Tecnica-Softbox';
+    var email = 'jefferson_ferreira@test.com';
+
+    this.timeout(timeOut);
+
+    test.before(function () {
+        driver = require('./driver').getDriver();
+        driver.manage().window().maximize();
+        indexPage = new IndexPage(driver);
+        indexPage.view()
+    });
+
+    test.after(function () {
+        if (driver)
+            driver.quit();
+    });
+
+    test.it('Registrar usuário', function (done) {
+        var indexPage = new IndexPage(driver);
+        var homePage = new HomePage(driver);
+        var registerPage = new RegisterPage(driver);
+
+        indexPage.view();
+        indexPage.clickRegister();
+        registerPage.informeLogin(usuario);
+        registerPage.informePassword(senha);
+        registerPage.informeConfirmation(senha);
+        registerPage.informeFirstName('Jefferson');
+        registerPage.informeLastName('Ferreira Faria');
+        registerPage.informeEmail(email);
+        registerPage.submit();
+        driver.sleep(1000);
+
+        homePage.verificaMsg('Your account has been activated. You can now log in.');
+
+        homePage.clickLogout();
+
+        done()
+    });
+
+    test.it('Realizar login', function (done) {
+
+        var loginPage = new LoginPage(driver);
+        homePage = new HomePage(driver);
+        indexPage = new IndexPage(driver);
+
+        indexPage.clickLogin();
+        loginPage.informeLogin(usuario);
+        loginPage.informePassword(senha);
+        loginPage.clickBtnLogin();
+        driver.sleep(1000);
+
+        homePage.verificaUsuarioLogado(usuario);
+
+        done();
+    });
+
+    test.it('Criar projeto', function (done) {
+
+        homePage = new HomePage(driver);
+        var projectPage = new ProjectPage(driver);
+        var newProjectPage = new NewProjectPage(driver);
+
+        homePage.clickProject();
+        projectPage.clickNewProject();
+        newProjectPage.informeName(projeto);
+        newProjectPage.informeDescription('Desrição do projeto de Teste');
+        newProjectPage.desmarcaTracker('Feature');
+        newProjectPage.desmarcaTracker('Support');
+        newProjectPage.clickCreate();
+        driver.sleep(1000);
+
+        homePage.verificaMsg('Successful creation.');
+
+        done();
+    });
+
+});
